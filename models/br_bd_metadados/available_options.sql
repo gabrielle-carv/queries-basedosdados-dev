5dd5f4cfd9502cd1d30d3a955903c3fbcff466fc
tricktx <patrick.tx@hotmail.com.br>
2023-03-22 11:52:21 -0300

    update br_bd_metadados available_options

--- models/br_bd_metadados/available_options.sql
+++ models/br_bd_metadados/available_options.sql
@@ -1,5 +1,5 @@
 SELECT
-  SAFE_CAST(elemento AS STRING) elemento,
-  SAFE_CAST(chave AS STRING) chave,
+  SAFE_CAST(element AS STRING) element,
+  SAFE_CAST(key AS STRING) key,
   SAFE_CAST(valor AS STRING) valor,
-FROM `basedosdados-dev.br_bd_metadados_staging.columns` AS t
+FROM `basedosdados-dev.br_bd_metadados_staging.columns` AS t 
