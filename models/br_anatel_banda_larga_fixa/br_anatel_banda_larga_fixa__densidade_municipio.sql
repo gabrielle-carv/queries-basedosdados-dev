7df6054f76afa4b9676ca1c37757ab4e0d173e18
Mauricio Fagundes <mauricio.fagundes@gmail.com>
2023-04-12 10:19:44 -0300

    fixing typos in sql and yaml

--- models/br_anatel_banda_larga_fixa/br_anatel_banda_larga_fixa__densidade_municipio.sql
+++ models/br_anatel_banda_larga_fixa/br_anatel_banda_larga_fixa__densidade_municipio.sql
@@ -5,6 +5,5 @@ SAFE_CAST(ano AS INT64) ano,
 SAFE_CAST(mes AS INT64) mes,
 SAFE_CAST(sigla_uf AS STRING) sigla_uf,
 SAFE_CAST(id_municipio AS STRING) id_municipio,
-SAFE_CAST(densidade AS STRING) densidade,
-
+SAFE_CAST(densidade AS STRING) densidade
 FROM basedosdados-dev.br_anatel_banda_larga_fixa_staging.densidade_municipio AS t
