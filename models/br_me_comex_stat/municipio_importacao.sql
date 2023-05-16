1ba3f0ca8d9ad2ee5d9a173d3587156f3531fb8d
folhesgabriel <pisagabriel09@gmail.com>
2023-05-16 07:46:51 -0300

    fix dbt models partitions

--- models/br_me_comex_stat/municipio_importacao.sql
+++ models/br_me_comex_stat/municipio_importacao.sql
@@ -3,7 +3,7 @@
     materialized='table',
      partition_by={
       "field": "ano",
-      "data_type": "int64",
+      "data_type": "int64"
     },
     cluster_by = ["mes","sigla_uf"],
     labels = {'project_id': 'basedosdados-dev', 'tema': 'economia'})
