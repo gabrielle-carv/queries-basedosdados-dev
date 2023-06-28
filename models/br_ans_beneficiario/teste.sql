34a02fc4de9bbd5a39961616de0752407cab9d31
Arthur Gusmão <arthurgusmao@id.uff.br>
2023-06-28 16:42:25 -0300

    fix: fixing model

--- models/br_ans_beneficiario/teste.sql
+++ models/br_ans_beneficiario/teste.sql
@@ -2,9 +2,7 @@
 
 {% for column in columns %}
 UPDATE `basedosdados-dev.br_ans_beneficiario.microdados_teste` AS tabela_mae
-SET {{ column }} = (
-  SELECT safe_cast(dicionario.chave AS STRING)
-  FROM `basedosdados-dev.br_ans_beneficiario.dicionario` AS dicionario
-  WHERE tabela_mae.{{ column }} = dicionario.valor
-);
+SET {{ column }} = safe_cast(dicionario.chave AS STRING)
+FROM `basedosdados-dev.br_ans_beneficiario.dicionario` AS dicionario
+WHERE tabela_mae.{{ column }} = dicionario.valor;
 {% endfor %}
