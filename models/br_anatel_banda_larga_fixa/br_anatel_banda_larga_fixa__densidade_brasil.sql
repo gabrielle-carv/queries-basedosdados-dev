58774d741947211e2d3d5f7882f108201ba32963
Patrick Teixeira <105399231+tricktx@users.noreply.github.com>
2023-06-28 11:31:17 -0300

    retirando virgula

--- models/br_anatel_banda_larga_fixa/br_anatel_banda_larga_fixa__densidade_brasil.sql
+++ models/br_anatel_banda_larga_fixa/br_anatel_banda_larga_fixa__densidade_brasil.sql
@@ -3,5 +3,5 @@
 SELECT 
 SAFE_CAST(ano AS INT64) ano,
 SAFE_CAST(mes AS INT64) mes,
-SAFE_CAST(densidade AS FLOAT64) densidade,
+SAFE_CAST(densidade AS FLOAT64) densidade
 FROM basedosdados-dev.br_anatel_banda_larga_fixa_staging.densidade_brasil AS t
