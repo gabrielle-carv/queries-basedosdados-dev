9fa379b1934f5cea4c9a6393b8aba3c64ddaf376
tricktx <patrick.tx@hotmail.com.br>
2023-07-24 12:39:13 -0300

    dbt alias

--- models/br_cnpq_bolsas/br_cnpq_bolsas__microdados.sql
+++ models/br_cnpq_bolsas/br_cnpq_bolsas__microdados.sql
@@ -1,16 +1,6 @@
-{{ config(
-    alias='microdados',
-    schema='br_cnpq_bolsas',
-    materialized='table',
-    partition_by={
-      "field": "ano",
-      "data_type": "int64",
-      "granularity": "year"
-    }
-) }}
+{{ config(alias='microdados', schema='br_cnpq_bolsas') }}
 
 SELECT 
-
 SAFE_CAST(ano AS INT64) ano,
 SAFE_CAST(processo AS STRING) processo,
 SAFE_CAST(data_inicio_processo AS DATE) data_inicio_processo,
