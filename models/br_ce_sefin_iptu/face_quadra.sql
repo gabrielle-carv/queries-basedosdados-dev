7e1562b763446a92a356d2628a8a3a76417490f1
tricktx <patrick.tx@hotmail.com.br>
2023-08-16 14:42:24 -0300

    geometria STRING

--- models/br_ce_sefin_iptu/face_quadra.sql
+++ models/br_ce_sefin_iptu/face_quadra.sql
@@ -2,7 +2,7 @@ SELECT
 SAFE_CAST(ano AS INT64) ano,
 SAFE_CAST(id_face_quadra AS STRING) id_face_quadra,
 SAFE_CAST(logradouro AS STRING) logradouro,
-SAFE.ST_GEOGFROMTEXT(geometry) geometria,
+SAFE_CAST(geometria AS STRING) geometria,
 SAFE_CAST(metrica AS STRING) metrica,
 SAFE_CAST(pavimentacao AS STRING) pavimentacao,
 SAFE_CAST(agua AS BOOL) agua,
