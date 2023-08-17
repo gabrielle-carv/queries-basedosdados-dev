198610ed543a592dc1616f502c8c3b67356a8593
Gabrielle Carvalho <77730866+gabrielle-carv@users.noreply.github.com>
2023-08-17 17:34:02 -0300

    incluindo restricao

--- models/br_me_cnpj/socios.sql
+++ models/br_me_cnpj/socios.sql
@@ -21,11 +21,19 @@ SELECT
     SAFE_CAST(tipo AS STRING) tipo,
     SAFE_CAST(nome AS STRING) nome,
     SAFE_CAST(documento AS STRING) documento,
-    SAFE_CAST(qualificacao AS STRING) qualificacao,
+    CASE 
+        WHEN qualificacao = '0' THEN '0'
+        ELSE SAFE_CAST(REGEXP_REPLACE(qualificacao, '^0', '') AS STRING)
+    END AS qualificacao,
+    SAFE_CAST(REGEXP_REPLACE(qualificacao, '^0', '') AS STRING) qualificacao,
     SAFE_CAST(data_entrada_sociedade AS DATE) data_entrada_sociedade,
     SAFE_CAST(REPLACE(id_pais,".0","") AS STRING) id_pais,
     SAFE_CAST(cpf_representante_legal AS STRING) cpf_representante_legal,
     SAFE_CAST(nome_representante_legal AS STRING) nome_representante_legal,
-    SAFE_CAST(qualificacao_representante_legal AS STRING) qualificacao_representante_legal,
+    CASE 
+        WHEN qualificacao_representante_legal = '0' THEN '0'
+        ELSE SAFE_CAST(REGEXP_REPLACE(qualificacao_representante_legal, '^0', '') AS STRING)
+    END AS qualificacao_representante_legal,
     SAFE_CAST(faixa_etaria AS STRING) faixa_etaria
 FROM basedosdados-dev.br_me_cnpj_staging.socios AS t
+WHERE qualificacao != "qualificacao"
