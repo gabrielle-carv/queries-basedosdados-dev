e16d2184659e53ac59561e3e58d4353827fc4398
tricktx <patrick.tx@hotmail.com.br>
2023-09-05 15:35:51 -0300

    model incremental dbt

--- models/br_mg_belohorizonte_smfa_iptu/br_mg_belohorizonte_smfa_iptu__iptu.sql
+++ models/br_mg_belohorizonte_smfa_iptu/br_mg_belohorizonte_smfa_iptu__iptu.sql
@@ -1,8 +1,16 @@
 {{ config(
     alias='iptu',
-    schema='br_mg_belohorizonte_smfa_iptu'
+    schema='br_mg_belohorizonte_smfa_iptu',
+    materialized='incremental',
+    partition_by={
+    "field": "mes",
+    "data_type": "int64",
+    "granularity": "month"},
+    cluster_by=['tipo_construtivo', 'tipo_ocupacao', 'tipologia'],
+    labels = {'project_id' : 'basedosdados-dev'}
 ) }}
 
+WITH iptu AS (
 SELECT 
 SAFE_CAST(ano AS INT64) ano,
 SAFE_CAST(mes AS INT64) mes,
@@ -11,13 +19,13 @@ SAFE_CAST(lote AS STRING) lote,
 SAFE_CAST(zoneamento AS STRING) zoneamento,
 SAFE_CAST(zona_homogenea AS STRING) zona_homogenea,
 SAFE_CAST(cep AS STRING) cep,
-SAFE_CAST(endereco AS STRING) endereco,
-SAFE_CAST(tipo_construtivo AS STRING) tipo_construtivo,
-SAFE_CAST(tipo_ocupacao AS STRING) tipo_ocupacao,
+INITCAP(endereco) endereco,
+INITCAP(tipo_construtivo) tipo_construtivo,
+INITCAP(tipo_ocupacao) tipo_ocupacao,
 SAFE_CAST(padrao_acabamento AS STRING) padrao_acabamento,
-SAFE_CAST(tipologia AS STRING) tipologia,
+INITCAP(tipologia) tipologia,
 SAFE_CAST(codigo_quantidade_economia AS STRING) codigo_quantidade_economia,
-SAFE_CAST(frequencia_coleta AS STRING) frequencia_coleta,
+INITCAP(frequencia_coleta) frequencia_coleta,
 SAFE_CAST(indicador_rede_telefonica AS BOOL) indicador_rede_telefonica,
 SAFE_CAST(indicador_meio_fio AS BOOL) indicador_meio_fio,
 SAFE_CAST(indicador_pavimentacao AS BOOL) indicador_pavimentacao,
@@ -27,7 +35,14 @@ SAFE_CAST(indicador_iluminacao_publica AS BOOL) indicador_iluminacao_publica,
 SAFE_CAST(indicador_rede_esgoto AS BOOL) indicador_rede_esgoto,
 SAFE_CAST(indicador_agua AS BOOL) indicador_agua,
 SAFE.ST_GEOGFROMTEXT(poligono) poligono,
-SAFE_CAST(fracao_ideal AS INT64) fracao_ideal,
-SAFE_CAST(area_terreno AS INT64) area_terreno,
-SAFE_CAST(area_construida AS INT64) area_construida,
-FROM basedosdados-dev.br_mg_belohorizonte_smfa_iptu_staging.iptu AS t
+SAFE_CAST(fracao_ideal AS FLOAT64) fracao_ideal,
+SAFE_CAST(area_terreno AS FLOAT64) area_terreno,
+SAFE_CAST(area_construida AS FLOAT64) area_construida,
+FROM basedosdados-dev.br_mg_belohorizonte_smfa_iptu_staging.iptu AS t)
+SELECT * FROM iptu
+
+{% if is_incremental() %}
+
+WHERE ano > (SELECT max(ano) FROM {{ this }}) and mes > (SELECT max(mes) FROM {{ this }})
+
+{% endif %}
