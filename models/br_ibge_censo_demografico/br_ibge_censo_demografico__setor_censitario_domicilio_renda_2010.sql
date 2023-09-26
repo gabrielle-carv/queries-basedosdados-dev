3de50c1d527a72775229552617dba7a335812270
Gagabrielle-carv <gabrielle.carvalho1@outlook.com>
2023-09-26 12:00:49 -0300

    add

--- models/br_ibge_censo_demografico/br_ibge_censo_demografico__setor_censitario_domicilio_renda_2010.sql
+++ models/br_ibge_censo_demografico/br_ibge_censo_demografico__setor_censitario_domicilio_renda_2010.sql
@@ -1,6 +1,6 @@
 {{ 
   config(
-    alias='microdados_domicilio_1970',
+    alias='setor_censitario_domicilio_renda_2010',
     schema='br_ibge_censo_demografico',
     materialized='table',
     partition_by={
@@ -9,12 +9,9 @@
     },
     )
  }}
-
 SELECT 
+    SAFE_CAST(id_setor_censitario AS STRING) id_setor_censitario,
     SAFE_CAST(sigla_uf AS STRING) sigla_uf,
-    SAFE_CAST(id_municipio AS STRING) id_municipio,
-    SAFE_CAST(id_domicilio AS STRING) id_domicilio,
-    SAFE_CAST(numero_familia AS INT64) numero_familia,
     SAFE_CAST(v001 AS INT64) v001,
     SAFE_CAST(v002 AS INT64) v002,
     SAFE_CAST(v003 AS INT64) v003,
@@ -28,13 +25,5 @@ SELECT
     SAFE_CAST(v011 AS INT64) v011,
     SAFE_CAST(v012 AS INT64) v012,
     SAFE_CAST(v013 AS INT64) v013,
-    SAFE_CAST(v014 AS INT64) v014,
-    SAFE_CAST(v015 AS INT64) v015,
-    SAFE_CAST(v016 AS INT64) v016,
-    SAFE_CAST(v017 AS INT64) v017,
-    SAFE_CAST(v018 AS INT64) v018,
-    SAFE_CAST(v019 AS INT64) v019,
-    SAFE_CAST(v020 AS INT64) v020,
-    SAFE_CAST(v021 AS INT64) v021,
-    SAFE_CAST(v054 AS INT64) v054
-FROM basedosdados-dev.br_ibge_censo_demografico_staging.microdados_domicilio_1970 AS t
+    SAFE_CAST(v014 AS INT64) v014
+from basedosdados-dev.br_ibge_censo_demografico_staging.setor_censitario_domicilio_renda_2010 as t
