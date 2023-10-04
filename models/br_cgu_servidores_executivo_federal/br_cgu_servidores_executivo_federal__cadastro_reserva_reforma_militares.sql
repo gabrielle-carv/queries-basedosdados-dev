6e64517efe2c37360d4941f36ffa425192fa1f5d
Pedro Castro <aspeddro@gmail.com>
2023-10-04 15:37:04 -0300

    dbt: br_cgu_servidores_executivo_federal rename tables (#318)
    * initial querys
    
    * add description
    
    * fixes
    
    * fixes
    
    * update descriptions
    
    * remove comments
    
    * dbt: br_cgu_servidores_executivo_federal rename tables
    
    * rm old sqls files
    
    * fix

--- models/br_cgu_servidores_executivo_federal/br_cgu_servidores_executivo_federal__cadastro_reserva_reforma_militares.sql
+++ models/br_cgu_servidores_executivo_federal/br_cgu_servidores_executivo_federal__cadastro_reserva_reforma_militares.sql
@@ -1,12 +1,13 @@
 {{
     config(
         schema = 'br_cgu_servidores_executivo_federal',
+        alias = 'cadastro_reserva_reforma_militares',
         materialized='table',
         partition_by={
             'field': 'ano',
             'data_type': 'int64',
             'range': {
-                "start": 2013,
+                "start": 2020,
                 "end": 2023,
                 "interval": 1
             }
@@ -32,86 +33,59 @@ select
     safe_cast(nome as string) nome,
     safe_cast(cpf as string) cpf,
     safe_cast(matricula as string) matricula,
+    safe_cast(id_tipo_aposentadoria as string) id_tipo_aposentadoria,
+    safe_cast(tipo_aposentadoria as string) tipo_aposentadoria,
+    (
+        case
+            when data_aposentadoria = "Não informada" then null
+            else parse_date('%d/%m/%Y', data_aposentadoria)
+        end
+    ) as data_aposentadoria,
     safe_cast(descricao_cargo as string) descricao_cargo,
-    safe_cast(classe_cargo as string) classe_cargo,
-    safe_cast(referencia_cargo as string) referencia_cargo,
-    safe_cast(padrao_cargo as string) padrao_cargo,
-    safe_cast(nivel_cargo as string) nivel_cargo,
-    safe_cast(sigla_funcao as string) sigla_funcao,
-    safe_cast(nivel_funcao as string) nivel_funcao,
-    safe_cast(funcao as string) funcao,
-    safe_cast(id_atividade as string) id_atividade,
-    safe_cast(atividade as string) atividade,
-    safe_cast(opcao_parcial as string) opcao_parcial,
     safe_cast(id_uorg_lotacao as string) id_uorg_lotacao,
     safe_cast(uorg_lotacao as string) uorg_lotacao,
     safe_cast(id_org_lotacao as string) id_org_lotacao,
     safe_cast(org_lotacao as string) org_lotacao,
     safe_cast(id_orgsup_lotacao as string) id_orgsup_lotacao,
     safe_cast(orgsup_lotacao as string) orgsup_lotacao,
-    safe_cast(id_uorg_exercicio as string) id_uorg_exercicio,
-    safe_cast(uorg_exercicio as string) uorg_exercicio,
-    safe_cast(id_org_exercicio as string) id_org_exercicio,
-    safe_cast(org_exercicio as string) org_exercicio,
-    safe_cast(id_orgsup_exercicio as string) id_orgsup_exercicio,
-    safe_cast(orgsup_exercicio as string) orgsup_exercicio,
     safe_cast(id_tipo_vinculo as string) id_tipo_vinculo,
     safe_cast(tipo_vinculo as string) tipo_vinculo,
     safe_cast(situacao_vinculo as string) situacao_vinculo,
-    (
-        case
-            when data_inicio_afastamento = "Não informada"
-            then null
-            else parse_date('%d/%m/%Y', data_inicio_afastamento)
-        end
-    ) as data_inicio_afastamento,
-    (
-        case
-            when data_termino_afastamento = "Não informada"
-            then null
-            else parse_date('%d/%m/%Y', data_termino_afastamento)
-        end
-    ) as data_termino_afastamento,
     safe_cast(regime_juridico as string) regime_juridico,
     safe_cast(jornada_trabalho as string) jornada_trabalho,
     (
         case
-            when data_ingresso_cargo_funcao = "Não informada"
-            then null
+            when data_ingresso_cargo_funcao = "Não informada" then null
             else parse_date('%d/%m/%Y', data_ingresso_cargo_funcao)
         end
     ) as data_ingresso_cargo_funcao,
     (
         case
-            when data_nomeacao_cargo_funcao = "Não informada"
-            then null
+            when data_nomeacao_cargo_funcao = "Não informada" then null
             else parse_date('%d/%m/%Y', data_nomeacao_cargo_funcao)
         end
     ) as data_nomeacao_cargo_funcao,
     (
         case
-            when data_ingresso_orgao = "Não informada"
-            then null
+            when data_ingresso_orgao = "Não informada" then null
             else parse_date('%d/%m/%Y', data_ingresso_orgao)
         end
     ) as data_ingresso_orgao,
     (
         case
-            when data_diploma_ingresso_servico_publico = "Não informada"
-            then null
+            when data_diploma_ingresso_servico_publico = "Não informada" then null
             else parse_date('%d/%m/%Y', data_diploma_ingresso_servico_publico)
         end
     ) as data_diploma_ingresso_servico_publico,
+    safe_cast(
+        documento_ingresso_servico_publico as string
+    ) documento_ingresso_servico_publico,
     safe_cast(diploma_ingresso_cargo_funcao as string) diploma_ingresso_cargo_funcao,
     safe_cast(diploma_ingresso_orgao as string) diploma_ingresso_orgao,
     safe_cast(
         diploma_ingresso_servico_publico as string
     ) diploma_ingresso_servico_publico,
-    safe_cast(
-        documento_ingresso_servico_publico as string
-    ) documento_ingresso_servico_publico,
-    (case when sigla_uf in ("-1", "-3") then null else sigla_uf end) as sigla_uf,
-    safe_cast(origem as string) origem,
 from
-    `basedosdados-dev.br_cgu_servidores_executivo_federal_staging.servidores_cadastro`
+    `basedosdados-dev.br_cgu_servidores_executivo_federal_staging.cadastro_reserva_reforma_militares`
     as t
+
