17851771be615e6de6452b6ef87772987d938d33
Arthur Gusmão <arthurgusmao@id.uff.br>
2023-11-30 17:57:38 -0300

    feat: refactoring

--- gists/create_yaml_file.py
+++ gists/create_yaml_file.py
@@ -3,7 +3,7 @@ import ruamel.yaml as yaml
 from utils import *
 import os
 from typing import List
-#test
+
 
 def create_yaml_file(arq_url, table_id, dataset_id, at_least: float = 0.05, unique_keys: List[str] = ["insert unique keys here"], mkdir=True) -> None:
     """
@@ -32,6 +32,11 @@ def create_yaml_file(arq_url, table_id, dataset_id, at_least: float = 0.05, uniq
         ```
 
     """
+    if mkdir:
+        os.makedirs(f"./models/{dataset_id}/", exist_ok=True)
+    else:
+        print("Directory for the new model has not been created, saving files in /queries-basedosadados-dev/gists/")    
+    
     yaml_obj = yaml.YAML(typ='rt')
     yaml_obj.indent(mapping=4, sequence=4, offset=2)
 
@@ -40,108 +45,47 @@ def create_yaml_file(arq_url, table_id, dataset_id, at_least: float = 0.05, uniq
     data.yaml_set_comment_before_after_key('models', before='\n\n')
     data['models'] = []            
 
-    if isinstance(table_id, str):
-        if mkdir:
-            os.makedirs(f"./models/{dataset_id}/", exist_ok=True)
-        else:
-            print("Directory for the new model has not been created, saving files in /queries-basedosadados-dev/gists/")    
-        
-        # If table_id is a string, assume a single input file
-        dataframe = sheet_to_df(arq_url)
+    if isinstance(table_id, str): 
+        table_id = [table_id]
+        arq_url = [arq_url]
+    # If table_id is a list, assume multiple input files
+    if not isinstance(arq_url, list) or len(arq_url) != len(table_id):
+        raise ValueError("The number of URLs or file paths must match the number of table IDs.")
+
+    for url, id in zip(arq_url, table_id):
+        unique_keys_copy = unique_keys.copy()
+        dataframe = sheet_to_df(url)
         conjunto = yaml.comments.CommentedMap()
-        conjunto['name'] = f'{table_id}'
-        conjunto['description'] = f"Insert `{table_id}` table description here"
-        conjunto['tests'] = create_unique_combination(unique_keys)
+        conjunto['name'] = f'{id}'
+        conjunto['description'] = f"Insert `{id}` table description here"
+        conjunto['tests'] = create_unique_combination(unique_keys_copy)
         conjunto['columns'] = []
 
         for _, row in dataframe.iterrows():
+            coluna = yaml.comments.CommentedMap()
+            coluna['name'] = row['name']
+            coluna['description'] = row['description']
+            tests = []
+            tests += create_not_null_proportion(at_least)
             if not pd.isna(row["directory_column"]):
-                name = row['name']
-                description = row['description']
                 directory = row["directory_column"]
-                coluna = yaml.comments.CommentedMap()
-                coluna['name'] = name
-                coluna['description'] = description
-                tests = []
-                tests += create_not_null_proportion(at_least)
                 tests += create_relationships(directory)
-                coluna['tests'] = tests
-                conjunto['columns'].append(coluna)
-            else:
-                name = row['name']
-                description = row['description']
-                coluna = yaml.comments.CommentedMap()
-                coluna['name'] = name
-                coluna['description'] = description
-                tests = []
-                tests += create_not_null_proportion(at_least)
-                coluna['tests'] = tests
-                conjunto['columns'].append(coluna)
+            coluna['tests'] = tests
+            conjunto['columns'].append(coluna)
+
 
         data['models'].append(conjunto)
 
-    elif isinstance(table_id, list):
-        # If table_id is a list, assume multiple input files
-        if not isinstance(arq_url, list) or len(arq_url) != len(table_id):
-            raise ValueError("The number of URLs or file paths must match the number of table IDs.")
-        if mkdir:
-            os.makedirs(f"./models/{dataset_id}/", exist_ok=True)
-        else:
-            print("Directory for the new model has not been created, saving files in /queries-basedosadados-dev/gists/")    
-        
-        for url, id in zip(arq_url, table_id):
-            unique_keys_copy = unique_keys.copy()
-            dataframe = sheet_to_df(url)
-            conjunto = yaml.comments.CommentedMap()
-            conjunto['name'] = f'{id}'
-            conjunto['description'] = f"Insert `{id}` table description here"
-            conjunto['tests'] = create_unique_combination(unique_keys_copy)
-            conjunto['columns'] = []
-
-            for _, row in dataframe.iterrows():
-                if not pd.isna(row["directory_column"]):
-                    name = row['name']
-                    description = row['description']
-                    directory = row["directory_column"]
-                    coluna = yaml.comments.CommentedMap()
-                    coluna['name'] = name
-                    coluna['description'] = description
-                    tests = []
-                    tests += create_not_null_proportion(at_least)
-                    tests += create_relationships(directory)
-                    coluna['tests'] = tests
-                    conjunto['columns'].append(coluna)
-                else:
-                    name = row['name']
-                    description = row['description']
-                    coluna = yaml.comments.CommentedMap()
-                    coluna['name'] = name
-                    coluna['description'] = description
-                    tests = []
-                    tests += create_not_null_proportion(at_least)
-                    coluna['tests'] = tests
-                    conjunto['columns'].append(coluna)
-
-            data['models'].append(conjunto)
-
+    if mkdir:
+        output_path = f"./models/{dataset_id}"   
     else:
-        raise TypeError("The table_id must be a string or a list.")
+        output_path = f"./gists/"  
 
-    if mkdir:       
-        with open(f"./models/{dataset_id}/schema.yml", 'w') as file:
-            yaml_obj.dump(data, file)
+    with open(f"{output_path}/schema.yml", 'w') as file:
+        yaml_obj.dump(data, file)
 
-        print(f"{table_id}")
-        create_models_from_architectures(arq_url,
-                                         output_dir=f"./models/{dataset_id}/",
-                                         dataset_id=dataset_id,
-                                         table_ids=table_id)               
-    else:
-        with open("./gists/schema.yml", 'w') as file:
-            yaml_obj.dump(data, file)
-
-        create_models_from_architectures(arq_url,
-                                         output_dir=f"./gists/{dataset_id}/",
-                                         dataset_id=dataset_id,
-                                         table_ids=table_id)           
+    create_models_from_architectures(arq_url,
+                                        output_dir=output_path,
+                                        dataset_id=dataset_id,
+                                        table_ids=table_id)                       
     print("Files successfully created!")
