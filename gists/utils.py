8e9a0e40f3872ec9d79e41ecade1e462711a8037
Arthur Gusmão <arthurgusmao@id.uff.br>
2023-12-01 18:57:34 -0300

    fix: smal fix

--- gists/utils.py
+++ gists/utils.py
@@ -36,6 +36,8 @@ def create_models_from_architectures(architectures, output_dir, dataset_id, tabl
 
         for architecture, table_id in zip(architectures,  table_ids):
             dataframe = sheet_to_df(architecture)
+            exclude = ['(excluido)', '(apagado)', '(deleteado)']
+            dataframe = dataframe[~dataframe['bigquery_type'].str.contains('|'.join(exclude))]
             dataframe.dropna(subset = ['bigquery_type'], inplace= True)
             with open(f"{output_dir}/{dataset_id}__{table_id}.sql", 'w') as file:
                 sql_config = "{{ config(alias=" + f"'{table_id}'," + "schema=" + f"'{dataset_id}'" + ") }}\n"
