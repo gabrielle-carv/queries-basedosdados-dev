42c945fb7b143914b45288ecafb598193e7f1cda
Arthur Gusmão <arthurgusmao@id.uff.br>
2023-12-08 17:06:39 -0300

    fix: fix sidra_to_dataframe

--- models/br_ibge_censo_2022/code/censo_2022_crawler.py
+++ models/br_ibge_censo_2022/code/censo_2022_crawler.py
@@ -1,12 +1,11 @@
-import os
-import re
-import logging
 import pandas as pd
 import requests
-from tqdm import tqdm
-from basedosdados import bd
+import os
+import basedosdados as bd
+import re
 from constants import constants
-
+import logging
+from tqdm import tqdm
 
 def municipalities_in_chunks(chunk_size: int = 50):
     query = """
@@ -15,27 +14,27 @@ def municipalities_in_chunks(chunk_size: int = 50):
     df_municipios = bd.read_sql(query, billing_project_id="casebd")
     input_list = list(df_municipios.id_municipio.unique())
 
-    return [input_list[i:i + chunk_size] for i in range(0, len(input_list), chunk_size)]
 
+    return [input_list[i:i + chunk_size] for i in range(0, len(input_list), chunk_size)]
 
 def sidra_to_dataframe(url: str) -> pd.DataFrame:
     try:
         response = requests.get(url=url)
-        response.raise_for_status()
-    except requests.exceptions.RequestException as e:
+        if response.status_code >= 400 and response.status_code <= 599:
+            logging.info(f"Tabela grande demais: {url}")
+            raise Exception(f"Erro de requisição: status code {response.status_code}")
+    except requests.exceptions.RequestException as e: 
         raise SystemExit(e)
     return pd.json_normalize(response.json())
 
-
 def rename_dataframe(df: pd.DataFrame) -> pd.DataFrame:
     df.columns = df.loc[0, :].values.flatten().tolist()
-    return df.iloc[1:, :]
-
+    return df.iloc[1: , :]
 
 def dataframe_to_parquet(df: pd.DataFrame, mkdir: bool, table_id: str) -> None:
     if mkdir:
         os.makedirs("/tmp/data/br_ibge_censo_2022/input", exist_ok=True)
-    df.to_parquet(path=f"/tmp/data/br_ibge_censo_2022/input/{table_id}.parquet", compression="gzip")
+    return df.to_parquet(path=f"/tmp/data/br_ibge_censo_2022/input/{table_id}.parquet", compression="gzip")
 
 
 if __name__ == "__main__":
@@ -45,19 +44,15 @@ if __name__ == "__main__":
         try:
             df = sidra_to_dataframe(v)
             df = rename_dataframe(df)
-            dataframe_to_parquet(df, mkdir=True, table_id=k)
-        except requests.exceptions.RequestException as e:
-            logging.info(f"Tabela grande demais: {v}")
-            logging.error(f"Erro de requisição: {e}")
-            continue
-
-        output_list = municipalities_in_chunks()
-        logging.info(f"Baixando dados em chunks da tabela: {k}")
-        for n in tqdm(range(len(output_list))):
-            munis = ",".join(str(value) for i, value in enumerate(output_list[n]))
-            url_nova = re.split(r"all(?=/v/)", v)
-            df = sidra_to_dataframe(url=f"{url_nova[0]}{munis}{url_nova[1]}")
-            df = rename_dataframe(df)
-            df_final = pd.concat([df_final, df])
-
-        dataframe_to_parquet(df_final, mkdir=True, table_id=k)
+            dataframe_to_parquet(df, mkdir = True, table_id=k )
+        except:
+            output_list = municipalities_in_chunks()
+            logging.info(f"Baixando dados em chunks da tabela: {k}")
+            for n in tqdm(range(len(output_list))):
+                munis = ""
+                munis += "".join(f"{value}" if i == 0 else f",{value}" for i, value in enumerate(output_list[n]))
+                url_nova = re.split(r"all(?=/v/)", v)
+                df = sidra_to_dataframe(url=f"{url_nova[0]}{munis}{url_nova[1]}")       
+                df = rename_dataframe(df)
+                df_final = pd.concat([df_final, df])
+            dataframe_to_parquet(df_final, mkdir = True, table_id=k )    
