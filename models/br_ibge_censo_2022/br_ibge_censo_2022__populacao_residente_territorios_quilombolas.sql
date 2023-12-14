2c4f30dbe5c8c079a25be8745628cc4ec444f980
Arthur Gusmão <arthurgusmao@id.uff.br>
2023-12-14 19:03:08 -0300

    feat(queries): last v2 models

--- models/br_ibge_censo_2022/br_ibge_censo_2022__populacao_residente_territorios_quilombolas.sql
+++ models/br_ibge_censo_2022/br_ibge_censo_2022__populacao_residente_territorios_quilombolas.sql
@@ -1,7 +1,7 @@
 {{ config(alias='populacao_residente_territorios_quilombolas',schema='br_ibge_censo_2022') }}
 SELECT
-SAFE_CAST(territorio_quilombola AS STRING) territorio_quilombola,
-SAFE_CAST(pessoas_residentes_territorios_quilombolas AS INT64) pessoas_residentes_territorios_quilombolas,
-SAFE_CAST(pessoas_quilombolas_residentes_territorios_quilombolas AS INT64) pessoas_quilombolas_residentes_territorios_quilombolas,
+SAFE_CAST(TRIM(REGEXP_EXTRACT(territorio_quilombola_por_unidade_da_federacao, r'([^\(]+)')) AS STRING) territorio_quilombola,
+SAFE_CAST(TRIM(REGEXP_EXTRACT(territorio_quilombola_por_unidade_da_federacao, r'\(([^)]+)\)')) AS STRING) sigla_uf,
+SAFE_CAST(pessoas_quilombolas_residentes_em_territorios_quilombolas_pessoas_ AS INT64) pessoas_residentes_territorios_quilombolas,
+SAFE_CAST(pessoas_residentes_em_territorios_quilombolas_pessoas_ AS INT64) pessoas_quilombolas_residentes_territorios_quilombolas,
 FROM basedosdados-dev.br_ibge_censo_2022_staging.populacao_residente_territorios_quilombolas AS t
-
