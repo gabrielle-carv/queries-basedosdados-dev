6ee39b03bae2e30a9aeb4eca1a8b009959a6d2d1
Arthur Gusmão <arthurgusmao@id.uff.br>
2023-12-15 19:43:13 -0300

    fix(queries): small fix on last 2 queries

--- models/br_ibge_censo_2022/br_ibge_censo_2022__populacao_grupos_idade_uf.sql
+++ models/br_ibge_censo_2022/br_ibge_censo_2022__populacao_grupos_idade_uf.sql
@@ -1,8 +1,11 @@
 {{ config(alias='populacao_grupos_idade_uf',schema='br_ibge_censo_2022') }}
 SELECT
-SAFE_CAST(ano AS INT64) ano,
-SAFE_CAST(sigla_uf AS STRING) sigla_uf,
-SAFE_CAST(grupo_idade AS STRING) grupo_idade,
-SAFE_CAST(populacao AS INT64) populacao,
+t2.sigla as sigla_uf,
+SAFE_CAST(grupo_de_idade AS STRING) grupo_idade,
+SAFE_CAST(`população` AS INT64) populacao,
 FROM basedosdados-dev.br_ibge_censo_2022_staging.populacao_grupos_idade_uf AS t
+left join `basedosdados.br_bd_diretorios_brasil.uf` t2
+on t.unidade_da_federacao = t2.nome
+
+
 
