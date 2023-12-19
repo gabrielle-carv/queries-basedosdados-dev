2bae1dbc9a13f756226d34eabc262e0efc8c5588
Arthur Gusmão <arthurgusmao@id.uff.br>
2023-12-19 17:45:06 -0300

    fix(queries): fixing join with directory

--- models/br_ibge_censo_2022/br_ibge_censo_2022__quilombolas_domicilios_pelo_menos_um_morador_municipio.sql
+++ models/br_ibge_censo_2022/br_ibge_censo_2022__quilombolas_domicilios_pelo_menos_um_morador_municipio.sql
@@ -1,6 +1,7 @@
 {{ config(alias='quilombolas_domicilios_pelo_menos_um_morador_municipio',schema='br_ibge_censo_2022') }}
 WITH ibge as (
 SELECT
+municipio,
 SAFE_CAST(TRIM(REGEXP_EXTRACT(municipio, r'([^\(]+)')) AS STRING) nome_municipio,
 SAFE_CAST(TRIM(REGEXP_EXTRACT(municipio, r'\(([^)]+)\)')) AS STRING) sigla_uf,
 SAFE_CAST(domicilios_particulares_permanentes_ocupados_com_pelo_menos_um_morador_quilombola_domicilios_ AS INT64) domicilios_pelo_menos_um,
@@ -10,8 +11,8 @@ SAFE_CAST(media_de_moradores_em_domicilios_particulares_permanentes_ocupados_com
 SAFE_CAST(media_de_moradores_quilombolas_em_domicilios_particulares_permanentes_ocupados_com_pelo_menos_um_morador_quilombola_pessoas_ AS FLOAT64) media_moradores_quilombolas_domicilios_particulares_pelo_menos_um,
 SAFE_CAST(localizacao_do_domicilio AS STRING) localizacao_domicilio,
 FROM basedosdados-dev.br_ibge_censo_2022_staging.quilombolas_domicilios_pelo_menos_um_morador_municipio AS t)
-select t2.id_municipio, ibge.* except(nome_municipio,sigla_uf) 
+select t2.cod as id_municipio, ibge.* except(municipio, nome_municipio,sigla_uf) 
 from ibge
-left join `basedosdados.br_bd_diretorios_brasil.municipio` t2
-on ibge.nome_municipio = t2.nome and ibge.sigla_uf = t2.sigla_uf
+left join `basedosdados-dev.br_ibge_censo_2022_staging.auxiliary_table` t2
+on ibge.municipio = t2.municipio
 
