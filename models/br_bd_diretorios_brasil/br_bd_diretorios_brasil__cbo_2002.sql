5a0378164dc6fef311b41d0c489c1971c6740824
Laura Amaral <laura.amaral@basedosdados.org>
2023-12-19 15:24:05 -0300

    fix: sql

--- models/br_bd_diretorios_brasil/br_bd_diretorios_brasil__cbo_2002.sql
+++ models/br_bd_diretorios_brasil/br_bd_diretorios_brasil__cbo_2002.sql
@@ -1,24 +1,10 @@
-/*
+{{ 
+  config(
+    alias='cbo_2002',    
+    schema='br_bd_diretorios_brasil',
+    materialized='table',)
+}}
 
-Query para publicar a tabela.
-
-Esse é o lugar para:
-    - modificar nomes, ordem e tipos de colunas
-    - dar join com outras tabelas
-    - criar colunas extras (e.g. logs, proporções, etc.)
-
-Qualquer coluna definida aqui deve também existir em `table_config.yaml`.
-
-# Além disso, sinta-se à vontade para alterar alguns nomes obscuros
-# para algo um pouco mais explícito.
-
-TIPOS:
-    - Para modificar tipos de colunas, basta substituir STRING por outro tipo válido.
-    - Exemplo: `SAFE_CAST(column_name AS NUMERIC) column_name`
-    - Mais detalhes: https://cloud.google.com/bigquery/docs/reference/standard-sql/data-types
-
-*/
-CREATE VIEW basedosdados-dev.br_bd_diretorios_brasil.cbo_2002 AS
 SELECT 
 SAFE_CAST(cbo_2002 AS STRING) cbo_2002,
 SAFE_CAST(descricao AS STRING) descricao,
