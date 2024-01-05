0fc0e2b483c88dd06aa19741a930cc08be2e359e
Arthur Gusmão <arthurgusmao@id.uff.br>
2024-01-05 19:23:31 -0300

    feat(models): small changes in models

--- models/br_ibge_censo_2022/br_ibge_censo_2022__populacao_residente_cor_raca_municipio.sql
+++ models/br_ibge_censo_2022/br_ibge_censo_2022__populacao_residente_cor_raca_municipio.sql
@@ -1,10 +1,10 @@
 {{ config(alias='populacao_residente_cor_raca_municipio',schema='br_ibge_censo_2022') }}
 SELECT
 SAFE_CAST(ano AS INT64) ano,
-SAFE_CAST(id_municipio AS STRING) id_municipio,
-SAFE_CAST(grupo_idade AS STRING) grupo_idade,
+SAFE_CAST(cod_ AS STRING) id_municipio,
+SAFE_CAST(idade AS STRING) grupo_idade,
 SAFE_CAST(sexo AS STRING) sexo,
-SAFE_CAST(cor_raca AS STRING) cor_raca,
-SAFE_CAST(populacao_residente AS INT64) populacao_residente,
+SAFE_CAST(cor_ou_raca AS STRING) cor_raca,
+SAFE_CAST(populacao_residente_pessoas_ AS INT64) populacao_residente,
 FROM basedosdados-dev.br_ibge_censo_2022_staging.populacao_residente_cor_raca_municipio AS t
 
