9dfa09941184000a86d598b07f36c2ad180b9000
tricktx <patrick.tx@hotmail.com.br>
2024-01-19 16:16:57 -0300

    Revert "Refactor SQL queries to capitalize author types and names"
    This reverts commit 1b3382cd9621961a33591fd4e22273809e5578a3.

--- models/br_camara_dados_abertos/br_camara_dados_abertos__proposicao_microdados.sql
+++ models/br_camara_dados_abertos/br_camara_dados_abertos__proposicao_microdados.sql
@@ -10,7 +10,17 @@
         "start": 1935,
         "end": 2023,
         "interval": 1}
-    })}}
+    },    
+    post_hook = ['CREATE OR REPLACE ROW ACCESS POLICY allusers_filter 
+                    ON {{this}}
+                    GRANT TO ("allUsers")
+                    FILTER USING (DATE_DIFF(DATE("{{ run_started_at.strftime("%Y-%m-%d") }}"), DATE(data), week) > 6)',
+          'CREATE OR REPLACE ROW ACCESS POLICY bdpro_filter 
+                ON  {{this}}
+                GRANT TO ("group:bd-pro@basedosdados.org", "group:sudo@basedosdados.org")
+                FILTER USING (True)']
+    )
+}}
 
 SELECT 
     SAFE_CAST(ano AS INT64) ano,
