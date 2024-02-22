4faec8754a5198550b586617ab4bbde42a05308d
Laura Amaral <laura.amaral@basedosdados.org>
2024-02-22 17:12:53 -0300

    fix: lint fix

--- models/br_ibge_criacao_municipios/br_ibge_criacao_municipios__microdados.sql
+++ models/br_ibge_criacao_municipios/br_ibge_criacao_municipios__microdados.sql
@@ -1,12 +1,13 @@
 {{ config(alias='microdados',schema='br_ibge_criacao_municipios') }}
 select
-safe_cast(id_municipio_criado as string) id_municipio_criado,
-safe_cast(nome_municipio_criado as string) nome_municipio_criado,
-safe_cast(id_municipio_origem as string) id_municipio_origem,
-t2.nome AS nome_municipio_origem,
-safe_cast(lei as string) lei,
-safe_cast(data_criacao as date) data_criacao,
-safe_cast(data_instalacao as date) data_instalacao,
+    safe_cast(id_municipio_criado as string) id_municipio_criado,
+    safe_cast(nome_municipio_criado as string) nome_municipio_criado,
+    safe_cast(id_municipio_origem as string) id_municipio_origem,
+    t2.nome as nome_municipio_origem,
+    safe_cast(lei as string) lei,
+    safe_cast(data_criacao as date) data_criacao,
+    safe_cast(data_instalacao as date) data_instalacao,
 from `basedosdados-dev.br_ibge_criacao_municipios_staging.microdados` as t1
-JOIN `basedosdados.br_bd_diretorios_brasil.municipio` AS t2
-ON t1.id_municipio_origem = t2.id_municipio
+join
+    `basedosdados.br_bd_diretorios_brasil.municipio` as t2
+    on t1.id_municipio_origem = t2.id_municipio
